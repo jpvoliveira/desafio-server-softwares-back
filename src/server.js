ebe748ccda157f1b846253c082411c6290e0c1dc
Joao Pedro <joao.pvo@aluno.ufop.edu.br>
2023-04-12 03:17:19 -0300

    Adicionando dependencia para erro de CORS

--- src/server.js
+++ src/server.js
@@ -2,11 +2,12 @@ import dotenv from 'dotenv';
 import express from 'express';
 import routes from './routes.js';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
-
 app.use(express.json());
+app.use(cors());
 app.use(routes)
 
 const dbUser = process.env.DB_USER
